c66a7597b5a599c046712639cd109eb5d1923376
anujshany <64464309+anujshany@users.noreply.github.com>
2020-11-08 18:21:11 +0530

    Add files via upload

---
+++ Ping.py
@@ -0,0 +1,94 @@
+from tkinter import *
+import random
+import time
+import pygame
+
+class Ball:
+
+    def __init__(self, canvas, paddle, color):
+        self.canvas = canvas
+        self.paddle = paddle
+        self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
+        self.canvas.move(self.id, 245, 100)
+        starts = [-3, -2, -1, 1, 2, 3]
+        random.shuffle(starts)
+        self.x = starts[0]
+        self.y = -3
+        self.canvas_height = self.canvas.winfo_height()
+        self.canvas_width = self.canvas.winfo_width()
+        self.hit_bottom = False
+
+    def hit_paddle(self, pos):
+        paddle_pos = self.canvas.coords(self.paddle.id)
+        if pos[2] >= paddle_pos[0] and pos[0] <= paddle_pos[2]:
+            if pos[3] >= paddle_pos[1] and pos[3] <= paddle_pos[3]:
+                return True
+        return False
+
+    def draw(self):
+        self.canvas.move(self.id, self.x, self.y)
+        pos = self.canvas.coords(self.id)
+        if pos[1] <= 0:
+            self.y = 3
+        if pos[3] >= self.canvas_height:
+            self.hit_bottom = True
+        if self.hit_paddle(pos) == True:
+            self.y = -3
+        if pos[0] <= 0:
+            self.x = 3
+        if pos[2] >= self.canvas_width:
+            self.x = -3
+class Paddle:
+    def __init__(self, canvas, color):
+        self.canvas = canvas
+        self.id = canvas.create_rectangle(0, 0, 100, 10, fill=color)
+        self.canvas.move(self.id, 450, 750)
+        self.x = 0
+        self.canvas_width = self.canvas.winfo_width()
+        self.canvas.bind_all('<KeyPress-Left>', self.turn_left)
+        self.canvas.bind_all('<KeyPress-Right>', self.turn_right)
+
+    def draw(self):
+        self.canvas.move(self.id, self.x, 0)
+        pos = self.canvas.coords(self.id)
+        if pos[0] <= 0:
+            self.x = 0
+        elif pos[2] >= self.canvas_width:
+            self.x = 0
+
+    def turn_left(self, evt):
+        self.x = -3
+
+    def turn_right(self, evt):
+        self.x = 3
+
+def restart ():
+    global paddle, ball
+    canvas.delete (ALL)
+    paddle = Paddle(canvas, 'blue')
+    ball = Ball(canvas, paddle, 'red')
+
+def main ():
+    global paddle, ball
+    tk.update()
+
+    paddle = Paddle(canvas, 'blue')
+    ball = Ball(canvas, paddle, 'red')
+
+    while 1:
+        if ball.hit_bottom == False:
+            ball.draw()
+            paddle.draw()
+        tk.update_idletasks()
+        tk.update()
+        time.sleep(0.01)
+
+tk = Tk()
+tk.title("Pong Game")
+tk.resizable(0, 0)
+tk.wm_attributes("-topmost", 1)
+canvas = Canvas(tk, width=1000, height=800, bd=0, highlightthickness=0)
+canvas.grid (row = 0)
+Button (tk, text = "Restart", command = restart).grid (row = 1)
+
+main ()
